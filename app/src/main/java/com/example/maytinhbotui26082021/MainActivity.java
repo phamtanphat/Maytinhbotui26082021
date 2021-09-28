40205e606876eea089a6ec63b8c08224643cd2bf
phamtanphat <phatdroid94@gmail.com>
2021-09-28 21:15:57 +0700

    finish

--- app/src/main/java/com/example/maytinhbotui26082021/MainActivity.java
+++ app/src/main/java/com/example/maytinhbotui26082021/MainActivity.java
@@ -63,5 +63,91 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mBtnNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Lấy dữ liệu từ 2 edittext
+                String textEdittext1 = mEdtSoThu1.getText().toString();
+                String textEdittext2 = mEdtSoThu2.getText().toString();
+
+                // Xem dữ liệu trong hệ thống
+//                Log.d("BBB","Số thứ 1 " + textEdittext1);
+//                Log.d("BBB","Số thứ 2 " + textEdittext2);
+
+                // kiểm tra nếu có giá trị rỗng
+                if (textEdittext1.isEmpty() || textEdittext2.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Bạn nhập chưa đầy đủ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Chuyển đổi kiểu dữ liệu từ string sang integer
+                Integer intSt1 = Integer.parseInt(textEdittext1);
+                Integer intSt2 = Integer.parseInt(textEdittext2);
+
+                int ketQua = intSt1 * intSt2;
+
+                mTvKetQua.setText("Kết quả : " + ketQua);
+            }
+        });
+
+        mBtnTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Lấy dữ liệu từ 2 edittext
+                String textEdittext1 = mEdtSoThu1.getText().toString();
+                String textEdittext2 = mEdtSoThu2.getText().toString();
+
+                // Xem dữ liệu trong hệ thống
+//                Log.d("BBB","Số thứ 1 " + textEdittext1);
+//                Log.d("BBB","Số thứ 2 " + textEdittext2);
+
+                // kiểm tra nếu có giá trị rỗng
+                if (textEdittext1.isEmpty() || textEdittext2.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Bạn nhập chưa đầy đủ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Chuyển đổi kiểu dữ liệu từ string sang integer
+                Integer intSt1 = Integer.parseInt(textEdittext1);
+                Integer intSt2 = Integer.parseInt(textEdittext2);
+
+                int ketQua = intSt1 - intSt2;
+
+                mTvKetQua.setText("Kết quả : " + ketQua);
+            }
+        });
+        mBtnChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Lấy dữ liệu từ 2 edittext
+                String textEdittext1 = mEdtSoThu1.getText().toString();
+                String textEdittext2 = mEdtSoThu2.getText().toString();
+
+                // Xem dữ liệu trong hệ thống
+//                Log.d("BBB","Số thứ 1 " + textEdittext1);
+//                Log.d("BBB","Số thứ 2 " + textEdittext2);
+
+                // kiểm tra nếu có giá trị rỗng
+                if (textEdittext1.isEmpty() || textEdittext2.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Bạn nhập chưa đầy đủ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                // Chuyển đổi kiểu dữ liệu từ string sang integer
+                float floatSt1 = Float.parseFloat(textEdittext1);
+                float floatSt2 = Float.parseFloat(textEdittext2);
+
+                if (floatSt2 == 0){
+                    Toast.makeText(MainActivity.this, "Không chia với giá trị 0", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                float ketQua = floatSt1 / floatSt2;
+
+                mTvKetQua.setText("Kết quả : " + ketQua);
+            }
+        });
+
     }
 }
